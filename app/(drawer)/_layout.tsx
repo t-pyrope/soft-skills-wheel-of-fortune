fded9fc2aebc231b0c0f0906ebe236c49f2842e2
Begaiym Adylbek <begaiym.adylbek@gmail.com>
2025-05-04 12:29:57 +0200

    Start adding custom drawer

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,12 +1,40 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItem,
+} from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
+import { Platform, TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import { i18n } from "@/i18n/config";
-import { Platform } from "react-native";
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      {props.state.routes.map((route, i) => {
+        const options = props.descriptors[route.key].options;
+
+        return (
+          <TouchableOpacity>
+            <DrawerItem
+              label={options.title ?? ""}
+              onPress={() => props.navigation.navigate(route.name)}
+            />
+          </TouchableOpacity>
+        );
+      })}
+    </DrawerContentScrollView>
+  );
+};
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer screenOptions={{ drawerType: "front" }}>
+      <Drawer
+        screenOptions={{ drawerType: "front" }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
         <Drawer.Screen
           name={"(stack)"}
           options={{
